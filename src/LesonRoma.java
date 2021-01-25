b53b137b9e2cc5bf2781f096d343d0b2192beba7
RomaYushin <yushin.khpi@gmail.com>
2021-01-25 23:42:20 +0200

    Commit from Roma

--- src/LesonRoma.java
+++ src/LesonRoma.java
@@ -2,6 +2,8 @@ import java.sql.SQLOutput;
 
 public class LesonRoma {
     public static void main (String args []) {
+	System.out.println("Hello git!!!");
+
 //        doSelection();
         doCircles();
     }
