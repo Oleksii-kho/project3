4af9c5838e1216ae5c25461ac19ebd48e108f2cb
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-01-25 23:56:17 +0200

    Добавил строку git2

--- src/LesonRoma.java
+++ src/LesonRoma.java
@@ -3,6 +3,7 @@ import java.sql.SQLOutput;
 public class LesonRoma {
     public static void main (String args []) {
 	System.out.println("Hello git!!!");
+	System.out.println("Hello git 2!!!");
 
 //        doSelection();
         doCircles();
