7ea2c90c4318f933820a7e9186b0bdaa54696989
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-05-01 23:19:22 +0300

    Добавлена 3-я строка git

---
+++ src/blinov/chapter1/var1/Task1.java
@@ -0,0 +1,14 @@
+package blinov.chapter1.var1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String st;
+
+        System.out.println("Введите ваше имя: ");
+        st = scan.next();
+        System.out.println("Приветствую вас, " + st);
+    }
+}
