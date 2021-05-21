0959e7b4e7451d3d5c7ca6cb4fcea12e8cdcf7f9
RomaYushin <yushin.khpi@gmail.com>
2021-05-22 00:55:17 +0300

    закончил проверять практику по главе 1 Блинова

--- src/blinov/chapter1/var2/Task7.java
+++ src/blinov/chapter1/var2/Task7.java
@@ -3,12 +3,18 @@ package blinov.chapter1.var2;
 // Вывести на консоль наибольший общий делитель и наименьшее общее кратное
 public class Task7 {
     public static void main(String[] args) {
+        var1();
+//        var2();
+    }
+
+    static void var1() {
         System.out.println("Для этой задачи рекомендуется вводить 2-3 числа");
 
         DataInput input = new DataInput();
         String [] areaString = input.enterQuantityAndString();
         int [] area = input.doStringToInt(areaString);
 
+        long start = System.nanoTime();
         int gcd = 1;                     // greatest common divisor - НОД
         int lcm = 1;                     // least common multiple - НОК
         int temp;
@@ -38,6 +44,8 @@ public class Task7 {
 
         System.out.println();
         System.out.println("НОД = " + gcd);
+        long finish = System.nanoTime();
+        System.out.println((finish-start));
 
         // Находим наибольшее число
         // НОК не может быть меньше, чем max
@@ -68,5 +76,32 @@ public class Task7 {
 
         System.out.println();
         System.out.println("НОК = " + lcm);
+    }
+
+    static void var2() {
+        System.out.println("Для этой задачи рекомендуется вводить 2-3 числа");
+
+        DataInput input = new DataInput();
+        String [] areaString = input.enterQuantityAndString();
+        int [] area = input.doStringToInt(areaString);
+
+        long start = System.nanoTime();
+        int gcd = area[0];
+
+        for (int i = 0; i < area.length-1; i++) {
+            int a = gcd;
+            int b = area[i+1];
+
+            while (b !=0) {
+                int tmp = a%b;
+                a = b;
+                b = tmp;
+            }
+            gcd = a;
+        }
+
+        System.out.println("\nНОД: " + gcd);
+        long finish = System.nanoTime();
+        System.out.println((finish-start));
     }
 }
